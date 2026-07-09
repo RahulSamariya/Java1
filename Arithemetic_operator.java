3ce731a0513297d18f5819e6c789c1102246d033
Shrey Shah <rahulsamariya008@gmail.com>
2026-07-09 23:33:57 +0530

    Arithemetic_operator
    ps_6

---
+++ Arithemetic_operator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Arithemetic_operator {
+
+    public static void main(String[] args) {
+
+        // arithemetic_Operator
+
+        int x = 10;
+        int y = 5;
+        int z;
+
+        //z = x + y
+        //z = x - y
+        //z = x * y
+        //z = x / y
+
+        z = x % y;
+
+        System.out.println(z);
+
+
+    }
+}
