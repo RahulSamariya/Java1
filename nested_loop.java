4a04496c59a7bb920866e4d4eb160a492dfffac2
Rahul <rahulsamariya008@gmail.com>
2026-07-28 16:15:59 +0530

    .
    .

---
+++ nested_loop.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class nested_loop {
+    public static void main(String[] args){
+
+        // Nested loops = A loop inside another loop
+        //                Usually used in the matrices and DSA
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the # of Rows: ");
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the # of Columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter the # symbol: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0 ; i < rows ; i++){
+            for(int j = 0 ; j < columns ; j++){
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+    }
+}
