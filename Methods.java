55954017b0d0de68c46231f749b564b6dd3e5828
Rahul <rahulsamariya008@gmail.com>
2026-07-30 23:16:54 +0530

    .
    .

--- Methods.java
+++ Methods.java
@@ -3,14 +3,39 @@ public class Methods {
 
         // method = a block of resusable code that is executed when called()
 
-        happyBirthday();
+        int age = 19;
+        System.out.println(ageCheck(age));
+        System.out.println(sqaure(3));
+        if (age >= 18) {
+            System.out.println("You may be sign Up!");
+        } else {
+            System.out.println("You must be 18+ to sign up!");
+        }
 
     }
-    static void happyBirthday() {
-            System.out.println("Happy Birthday to you!");
-            System.out.println("Happy Birthday dear you!");
-            System.out.println("You are x years old!");
-            System.out.println("Happy Birthday to you!\n");
 
+    static void happyBirthday(String name, int age) {
+        System.out.println("Happy Birthday to you!");
+        System.out.printf("Happy Birthday dear %s!\n", name);
+        System.out.printf("You are %d years old!\n", age);
+        System.out.println("Happy Birthday to you!\n");
+
+    }
+    static double sqaure(double number) {
+        return number * number;
+    }
+    static double cube(double number) {
+        return number * number * number;
+    }
+    static String getFullName(String first, String last) {
+        return first + " " + last;
+    }
+    static boolean ageCheck(int age) {
+        if(age >= 18){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
---
+++ Overloaded_method.java
@@ -0,0 +1,24 @@
+public class Overloaded_method {
+    public static void main(String[] args) {
+
+        // Overloaded methods = methods that share the same name;
+        //                      But different parameters
+        //                      Signature = name + parameter
+
+        System.out.println(((add(1,2,3))));
+
+    }
+
+    static double add(double a, double b){
+        return a + b;
+    }
+    static double add(double a, double b, double c){
+        return a + b + c;
+    }
+    static String bakePizza(String bread){
+        return bread + " Pizza";
+    }
+    static String bakePizza(String bread, String cheese){
+        return bread + " Pizza";
+    }
+}
