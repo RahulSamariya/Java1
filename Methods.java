61322f5eb064db1004f4ffb25e2dbf931d543b71
Rahul <rahulsamariya008@gmail.com>
2026-07-30 22:05:40 +0530

    .
    .

---
+++ Methods.java
@@ -0,0 +1,16 @@
+public class Methods {
+    public static void main() {
+
+        // method = a block of resusable code that is executed when called()
+
+        happyBirthday();
+
+    }
+    static void happyBirthday() {
+            System.out.println("Happy Birthday to you!");
+            System.out.println("Happy Birthday dear you!");
+            System.out.println("You are x years old!");
+            System.out.println("Happy Birthday to you!\n");
+
+    }
+}
